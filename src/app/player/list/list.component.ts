f977ee795163a95556f499c21f6fa3551da92682
Pawel Paszki <pawelpaszki@gmail.com>
2019-04-04 22:23:20 +0100

    further work done on song list

--- src/app/player/list/list.component.ts
+++ src/app/player/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
 // import { FileService } from 'src/app/services/file-service';
 import { FileChooser } from '@ionic-native/file-chooser/ngx';
 import { Media, MediaObject } from '@ionic-native/media/ngx';
@@ -8,14 +8,15 @@ import { Media, MediaObject } from '@ionic-native/media/ngx';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements AfterContentInit {
 
-  public songs: String[] = ['song1', 'song2', 'song3', 'song4', 'song5', 'song6', 'song7', 'song8'];
+  public songs: String[] = ['song1 by some random artist', 'song2  by some random artis', 'song3  by some random artis', 'song4  by some random artis', 
+                            'song5  by some random artis', 'song6  by some random artis', 'song7  by some random artis', 'song8  by some random artis'];
   public displayMode: string = 'detail';
 
   constructor(public fileChooser: FileChooser, public media: Media, /*public fileService: FileService*/) { }
 
-  ngOnInit() {
+  ngAfterContentInit() {
     console.log(this.songs);
   }
 
@@ -25,5 +26,15 @@ export class ListComponent implements OnInit {
 
   delete(index: number) {
     this.songs.splice(index,1);
+  }
+
+  reorderItems(indexes) {
+    console.log(indexes);
+    let from = indexes.from;
+    let to = indexes.to;
+    let fromItem = this.songs[from];
+    let toItem = this.songs[to];
+    this.songs[to] = fromItem;
+    this.songs[from] = toItem;
   }
 }
