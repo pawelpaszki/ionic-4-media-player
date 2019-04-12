c0f778c76d1857dbc3fdf73f80e1008cdd75b807
Pawel Paszki <pawelpaszki@gmail.com>
2019-04-12 18:28:40 +0100

    init work on player component done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,8 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.overlaysWebView(true);
+      this.statusBar.backgroundColorByHexString('#00000000');
       this.splashScreen.hide();
     });
   }
--- src/app/player/mediaplayer/mediaplayer.component.ts
+++ src/app/player/mediaplayer/mediaplayer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
+import { Platform } from '@ionic/angular';
+import { UtilService } from 'src/providers/util.service';
+import * as Data from '../../../AppConstants';
 
 @Component({
   selector: 'app-mediaplayer',
@@ -7,8 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaplayerComponent implements OnInit {
 
-  constructor() { }
+  public shuffle: boolean = false; // save and retrieve from local storage
+  public isPlaying: boolean = false;
+  public progress: number = 0;
+  public max: number = 100;
+  public constants = Data;
+  public repeatModes: string[] = Object.values(this.constants.REPEAT_MODES);
+  public repeat: string = this.repeatModes[0];// TODO get from persistence service later
+  public currentRepeatIndex: number = 0;
+
+  constructor(public keyboard: Keyboard, public platform: Platform, public util: UtilService) { }
 
   ngOnInit() {}
+
+  stop() {
+    console.log('stop');
+  }
+
+  play() {
+    console.log('play');
+  }
+
+  previous() {
+    console.log('previous');
+  }
+
+  next() {
+    console.log('next');
+  }
+
+  updateProgress(event) {
+    this.progress = event.detail.value;
+  }
+
+  toggleShuffle() {
+    this.shuffle = !this.shuffle; // persist later
+  }
+
+  changeRepeatMode() {
+    if (this.repeatModes !== undefined && this.repeatModes !== null && this.repeatModes.length > 1) {
+      const currentRepeatIndex = this.repeatModes.indexOf(this.repeat);
+      if (currentRepeatIndex === this.repeatModes.length - 1) {
+        this.repeat = this.repeatModes[0];
+      } else {
+        this.repeat = this.repeatModes[currentRepeatIndex + 1];
+      }
+    }
+  }
 
 }
